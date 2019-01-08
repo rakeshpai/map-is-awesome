45f22f64241528adf112f339bac85753873234e3
Rakesh Pai <rakeshpai@gmail.com>
2019-01-08 22:26:32 +0530

    Updates

---
+++ precache-manifest.440d6bfb97883c88b574950ce6689d57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9233c6017bc76f685b88ef88b3b7216e",
+    "url": "/static/media/orion.9233c601.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91b68947a2b7589d673d",
+    "url": "/static/js/main.91b68947.chunk.js"
+  },
+  {
+    "revision": "fcbf09ab568b911b2128",
+    "url": "/static/js/1.fcbf09ab.chunk.js"
+  },
+  {
+    "revision": "fcbf09ab568b911b2128",
+    "url": "/static/css/1.d4316c29.chunk.css"
+  },
+  {
+    "revision": "d83f593d22ea7e10150d3c9fce33b37d",
+    "url": "/index.html"
+  }
+];
